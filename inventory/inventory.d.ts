a9b664caa9d158cf3782867e713b1fce1a8cc414
Michael Ferris <mike.ferris@hotmail.com>
2014-10-09 23:28:44 -0400

    added faked definitions in the file

---
+++ inventory/inventory.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for inventory v0.0.0
+// Project: 
+// Definitions by: Michael Ferris <https://github.com/Cellule/>
+// Definitions: https://github.com/my-koop/type.definitions
+
+
+declare module "mykoop-inventory" {
+  
+  function get(id: string) : string;
+
+  export = get;
+}
+
